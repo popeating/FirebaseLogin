9fdeec4df07ac1a72fe8da22b4b07d9c6171edbf
popeating <pop@popeating.it>
2020-08-04 16:50:42 +0200

    cleaned translation

--- const/translation.js
+++
@@ -1,8 +0,0 @@
-import i18n from 'i18n-js';
-
-i18n.translations = {
-  it: { mappa: 'Mappa', clickme: 'Cliccami' },
-  en: { mappa: 'Map', clickme: 'Click me' },
-};
-
-export default i18n.translations;
